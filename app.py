00845508422eb1c21b656112ce0bdb3a887408d8
koki <144724757+helpkoki@users.noreply.github.com>
2025-05-14 21:39:39 +0200

    working code

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def recognize_image():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host="0.0.0.0", port=port)
-    
+# Note: this code works on railway   
